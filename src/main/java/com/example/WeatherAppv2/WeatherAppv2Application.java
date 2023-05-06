293885c4f3265f8d5ac6371d8f963c9c5b4dec2e
Alexandre STANISIERE <alexandre.stanisiere@hotmail.fr>
2023-05-06 14:54:58 +0200

    First attempt of creating a frontend. Adding of a index.html in the /static and adding of a GetMapping int the class WeatherController

--- src/main/java/com/example/WeatherAppv2/WeatherAppv2Application.java
+++ src/main/java/com/example/WeatherAppv2/WeatherAppv2Application.java
@@ -1,13 +1,17 @@
 package com.example.WeatherAppv2;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.PageAttributes.MediaType;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -26,9 +30,16 @@ public class WeatherAppv2Application {
     @RestController
     public class WeatherController {
 
-        @GetMapping("/weather")
+    	@GetMapping("/")
+    	public String getHomePage() {
+    	    return "index.html";
+    	}
+
+    	
+        @GetMapping("/Weather")
         public String getWeather(@RequestParam(value = "city", defaultValue = "Wien") String city) throws IOException {
             String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY;
+
 
             // HTTP Request
             URL obj = new URL(url);
